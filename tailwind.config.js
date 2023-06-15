8f293566cf5a8a51f2044daee6478c4fe198427e
Mohamed Ashref Ben Abdallah <mohamedashrefbenabdallah@gmail.com>
2023-06-15 10:56:06 +0100

    Added tailwind css 🎊

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config}  */
+
+/* eslint-env node */
+
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
