3b3987f43e1019efa3695e8b1911b84c743e2d64
Mohamed Ashref Ben Abdallah <mohamedashrefbenabdallah@gmail.com>
2023-06-20 00:36:41 +0100

    Navbar done ✨

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,11 +5,29 @@
 </script>
 
 <template>
-  <header>
-    <h1>This is the header component</h1>
+  <header class="flex items-center justify-between mx-8 my-4">
+    <img src="../assets/logo/PrepaHub_logo_text.svg" alt="" srcset="" />
+
+    <ul class="hidden items-center gap-4 md:flex">
+      <li>Maison</li>
+      <li>Calculator</li>
+      <li>Aide</li>
+    </ul>
+
+    <div class="hidden gap-4 items-center justify-center md:flex">
+      <p>Log in</p>
+      <button style="border-radius: 35px 35px 0 35px" class="bg-black px-6 py-2 text-white rounded">
+        Sign up
+      </button>
+    </div>
+    <div class="block w-8 md:hidden">
+      <img
+        src="https://img.uxwing.com/wp-content/themes/uxwing/download/web-app-development/hamburger-menu-icon.png"
+        alt=""
+        srcset=""
+      />
+    </div>
   </header>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
